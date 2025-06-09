4753daec6a1780de489606f1c10e1687d1c0f272
victorvinh <baothuong0408@gmail.com>
2025-06-09 19:19:27 +0700

    update README.md with new project details and images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-import {withSentryConfig} from '@sentry/nextjs';
-import path from 'path'
-import type { NextConfig } from 'next'
+import path from 'path';
+import { withSentryConfig } from '@sentry/nextjs';
+import type { NextConfig } from 'next';
+
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    authInterrupts: true,
-  },
   images: {
     remotePatterns: [
       {
